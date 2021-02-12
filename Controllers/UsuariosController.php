93b16cb6b42276a8daa5aa5f05a02629153edeb5
honorabetto <honorabetto@gmail.com>
2021-02-11 22:24:07 -0600

    se quita codigo excedente para otras acciones que no se implementaron

--- Controllers/UsuariosController.php
+++ Controllers/UsuariosController.php
@@ -43,39 +43,6 @@ if(isset($_REQUEST['action']))
             }
             require_once("../Views/RegistrarView.php");
             break;
-
-
-
-        case 'actualizar':
-            $alm->__SET('id',              $_REQUEST['id']);
-            $alm->__SET('Nombre',          $_REQUEST['Nombre']);
-            $alm->__SET('Apellido',        $_REQUEST['Apellido']);
-            $alm->__SET('Sexo',            $_REQUEST['Sexo']);
-            $alm->__SET('FechaNacimiento', $_REQUEST['FechaNacimiento']);
-
-            $model->Actualizar($alm);
-            header('Location: index.php');
-            break;
-
-        
-
-        case 'eliminar':
-            $model->Eliminar($_REQUEST['id']);
-            header('Location: index.php');
-            break;
-
-        case 'editar':
-            $alm = $model->Obtener($_REQUEST['id']);
-            break;
     }
-
-
 }
-
-
-//$user=new UsuariosModel();
-//$datos=$user->GetUsuarios();
- 
-//Llamada a la vista
-
 ?>
